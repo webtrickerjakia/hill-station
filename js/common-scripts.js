bf437ab0d9e02dce5dab940cfc8c69bb758b79a6
webtrickerjakia <jarin3006@gmail.com>
2024-05-02 11:45:00 +0600

    js code change

--- js/common-scripts.js
+++ js/common-scripts.js
@@ -32,6 +32,45 @@
         }
 
 
+
+
+// Function to add "head shake" animation
+function addHeadShakeAnimation() {
+    var heroThumb = document.getElementById('heroThumb');
+    heroThumb.style.animation = 'shake 1s';
+}
+
+// Call the function to add the animation
+addHeadShakeAnimation();
+
+// Function to add "head shake" animation when the section comes into view
+function addIntersectionObserverAnimation(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            var thumbBanner = document.getElementById('heroThumb1');
+            thumbBanner.classList.add('head-shake');
+        }
+    });
+}
+
+// Set up the Intersection Observer
+const options = {
+    threshold: 0.5 // Trigger animation when at least 50% of the element is visible
+};
+
+const observer = new IntersectionObserver(addIntersectionObserverAnimation, options);
+const findUsBanner = document.getElementById('find-us-banner');
+observer.observe(findUsBanner);
+
+
+// slide in left to right
+
+
+
+
+
+
+
 // map tab
 
         $('.map').not(':first').hide();
@@ -47,31 +86,6 @@
             // Show the map container corresponding to the targetMapId
             $('#' + targetMapId).show();
         });
-
-
-// seacrhing :
-
-// JavaScript for filtering location items based on search input
-// document.getElementById('locationName').addEventListener('input', function () {
-//     filterLocations();
-// });
-
-// function filterLocations() {
-//     let input, filter, locations, locationItem, i, txtValue;
-//     input = document.getElementById('locationName');
-//     filter = input.value.toUpperCase();
-//     locations = document.querySelectorAll('.csl-list-item');
-
-//     locations.forEach(location => {
-//         locationItem = location.querySelector('h4');
-//         txtValue = locationItem.textContent || locationItem.innerText;
-//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//             location.style.display = '';
-//         } else {
-//             location.style.display = 'none';
-//         }
-//     });
-// }
 
 
          // Function to show dropdown based on search input
@@ -137,9 +151,9 @@
 
 
        // Event listener for reset button
-document.getElementById("resetButton").addEventListener("click", function() {
+   document.getElementById("resetButton").addEventListener("click", function() {
     resetForm();
-});
+   });
 
 // Function to reset form and reload page
 function resetForm() {
@@ -152,70 +166,6 @@ function resetForm() {
 
 
 
-// // সার্চ বক্সে লিখা দেখার জন্য ড্রপডাউন দেখানো
-// function showDropdown() {
-//     var input, filter, ul, li, a, i;
-//     input = document.getElementById("locationName");
-//     filter = input.value.toUpperCase();
-//     ul = document.getElementById("locationDropdown");
-//     li = ul.getElementsByTagName("div");
-//     for (i = 0; i < li.length; i++) {
-//         a = li[i].getElementsByTagName("h4")[0];
-//         if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-//             li[i].style.display = "";
-//         } else {
-//             li[i].style.display = "none";
-//         }
-//     }
-//     ul.style.display = "block"; // ড্রপডাউন দেখানো
-// }
-
-// // লোকেশন সিলেক্ট করা এবং সার্চ বাটন ক্লিক করা
-
-
-// function filterLocations() {
-//     var input, filter, ul, li, a, i;
-//     input = document.getElementById("locationName");
-//     filter = input.value.toUpperCase();
-//     ul = document.getElementById("locations-near-you");
-//     li = ul.getElementsByClassName("csl-list-item");
-//     for (i = 0; i < li.length; i++) {
-//         a = li[i].getElementsByTagName("h4")[0]; // Change this line
-//         if (a.innerText.toUpperCase().indexOf(filter) > -1) {
-//             li[i].style.display = "";
-//         } else {
-//             li[i].style.display = "none";
-//         }
-//     }
-// }
-
-// // রিসেট বাটন ক্লিক করা
-// function resetForm() {
-//     document.getElementById("locationSearchForm").reset();
-//     var locationResults = document.getElementById("locationResults");
-//     var allLocations = locationResults.getElementsByClassName("csl-list-item");
-//     for (var i = 0; i < allLocations.length; i++) {
-//         allLocations[i].style.display = "";
-//     }
-// }
-
-// // সার্চ বাটন এবং রিসেট বাটনের ইভেন্ট হ্যান্ডলিং
-// document.getElementById("locationSearchForm").addEventListener("submit", function(event) {
-//     event.preventDefault(); // ফর্ম সাবমিট প্রথমে পৃষ্ঠা রিলোড না হওয়ার জন্য
-//     filterLocations();
-// });
-
-// document.getElementById("resetButton").addEventListener("click", function() {
-//     resetForm();
-// });
-
-
-// document.getElementById("searchButton").addEventListener("click", function(event) {
-//     event.preventDefault(); // ফর্ম সাবমিট প্রথমে পৃষ্ঠা রিলোড না হওয়ার জন্য
-//     filterLocations();
-// });
-
-
 
 
 
@@ -223,40 +173,6 @@ function resetForm() {
 
 // text animation
 
-var $animation_elements = $('.animation-element');
-var $window = $(window);
-var animation_triggered = false; // Flag to track animation trigger
-var last_scroll_top = 0;
-
-function check_if_in_view() {
-    var window_top_position = $window.scrollTop();
-    var window_height = $window.height();
-
-    $.each($animation_elements, function () {
-        var $element = $(this);
-        var element_height = $element.outerHeight();
-        var element_top_position = $element.offset().top;
-        var element_bottom_position = element_top_position + element_height;
-
-        // Check if the element is in the viewport and the user is scrolling down
-        if (element_bottom_position >= window_top_position && element_top_position <= window_top_position + window_height && window_top_position > last_scroll_top) {
-            $element.addClass('in-view');
-            if (!animation_triggered) {
-                animation_triggered = true;
-            }
-        } else {
-            // If animation has not been triggered yet, keep the animation class applied
-            if (!animation_triggered) {
-                $element.removeClass('in-view');
-            }
-        }
-    });
-
-    last_scroll_top = window_top_position;
-}
-
-$window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll', check_if_in_view);
 
 
 
@@ -273,34 +189,6 @@ $window.trigger('scroll', check_if_in_view);
         
 
 
-
-// Function to add "head shake" animation
-function addHeadShakeAnimation() {
-    var heroThumb = document.getElementById('heroThumb');
-    heroThumb.style.animation = 'shake 1s';
-}
-
-// Call the function to add the animation
-addHeadShakeAnimation();
-
-// Function to add "head shake" animation when the section comes into view
-function addIntersectionObserverAnimation(entries, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            var thumbBanner = document.getElementById('heroThumb1');
-            thumbBanner.classList.add('head-shake');
-        }
-    });
-}
-
-// Set up the Intersection Observer
-const options = {
-    threshold: 0.5 // Trigger animation when at least 50% of the element is visible
-};
-
-const observer = new IntersectionObserver(addIntersectionObserverAnimation, options);
-const findUsBanner = document.getElementById('find-us-banner');
-observer.observe(findUsBanner);
 
 
 		
@@ -401,6 +289,48 @@ $('#age-verify .window button.no').on('click', function() {
 $('#age-verify .window button.back').on('click', function() {
   window.history.back();
 });
+
+
+
+
+// slide
+// var $animation_elements = $('.animation-element');
+// var $window = $(window);
+// var animation_triggered = false; // Flag to track animation trigger
+// var last_scroll_top = 0;
+
+// function check_if_in_view() {
+//     var window_top_position = $window.scrollTop();
+//     var window_height = $window.height();
+
+//     $.each($animation_elements, function () {
+//         var $element = $(this);
+//         var element_height = $element.outerHeight();
+//         var element_top_position = $element.offset().top;
+//         var element_bottom_position = element_top_position + element_height;
+
+//         // Check if the element is in the viewport and the user is scrolling down
+//         if (element_bottom_position >= window_top_position && element_top_position <= window_top_position + window_height && window_top_position > last_scroll_top) {
+//             $element.addClass('in-view');
+//             if (!animation_triggered) {
+//                 animation_triggered = true;
+//             }
+//         } else {
+//             // If animation has not been triggered yet, keep the animation class applied
+//             if (!animation_triggered) {
+//                 $element.removeClass('in-view');
+//             }
+//         }
+//     });
+
+//     last_scroll_top = window_top_position;
+// }
+
+// $window.on('scroll resize', check_if_in_view);
+// $window.trigger('scroll', check_if_in_view);
+
+
+
 
 
 
